98d8586852534fd3bddf9a8a3a9b1d6d7e5adc66
ArunKarthikAnandKumar <137405963+ArunKarthikAnandKumar@users.noreply.github.com>
2026-01-31 19:11:22 +0530

    feat:add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello Devops')
+console.log("I'm Learning CI/CD Pipelines using Github Actions")
---
+++ tests.js
@@ -0,0 +1,4 @@
+console.log('Starting Tests')
+setTimeout(()=>{
+    console.log('Waiting 3 s')
+},3000)
